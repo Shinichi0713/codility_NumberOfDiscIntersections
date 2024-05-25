744cffc401a24dc061286cc83ccff33d9284a63e
Shinichi Yoshinaga <61480734+Shinichi0713@users.noreply.github.com>
2024-05-26 08:47:47 +0900

    Add files via upload

---
+++ solution.py
@@ -0,0 +1,17 @@
+
+def solution(A):
+    events = []
+    # 先に軸との交差点を求める
+    for i, a in enumerate(A):
+        events += [(i-a, +1), (i+a, -1)]
+    events.sort(key=lambda x: (x[0], -x[1]))
+    intersections, active_circles = 0, 0
+    for _, circle_count_delta in events:
+        print((circle_count_delta > 0))
+        intersections += active_circles * (circle_count_delta > 0)
+        active_circles += circle_count_delta
+        if intersections > 10E6:
+            return -1
+    return intersections
+
+solution([3,5,1,6,1])
